6de34450e52f898ce3cd5a4cfe546b5626e42931
Djoiusis <jihaadbenyedder@gmail.com>
2024-11-13 11:59:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from test import process_cv, generate_cv, get_logs, get_temp_log_path
+from test import main as process_cv, generate_cv, get_logs, get_temp_log_path
 import os
 
 # Page layout
